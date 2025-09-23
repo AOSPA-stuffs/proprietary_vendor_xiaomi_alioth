3d62fdb23bb92ec55955dc50ba33ce16fb9eba7e
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-09-23 16:18:39 +0000

    apollo: Drop libril-qcril-hook-oem.so
    Requires libril, we disabled that target, safe to assume only non-qcrild
    uses this
    
    Change-Id: I5d868306c7ef72fb863009f0f3db72a2e01bf1fd
    Signed-off-by: peternmuller <pm563838@gmail.com>

--- apollo/apollo-vendor.mk
+++ apollo/apollo-vendor.mk
@@ -901,7 +901,6 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/apollo/proprietary/vendor/lib64/libril-qc-logger.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libril-qc-logger.so \
     vendor/xiaomi/apollo/proprietary/vendor/lib64/libril-qc-ltedirectdisc.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libril-qc-ltedirectdisc.so \
     vendor/xiaomi/apollo/proprietary/vendor/lib64/libril-qc-radioconfig.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libril-qc-radioconfig.so \
-    vendor/xiaomi/apollo/proprietary/vendor/lib64/libril-qcril-hook-oem.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libril-qcril-hook-oem.so \
     vendor/xiaomi/apollo/proprietary/vendor/lib64/librilqmiservices.so:$(TARGET_COPY_OUT_VENDOR)/lib64/librilqmiservices.so \
     vendor/xiaomi/apollo/proprietary/vendor/lib64/librmp.so:$(TARGET_COPY_OUT_VENDOR)/lib64/librmp.so \
     vendor/xiaomi/apollo/proprietary/vendor/lib64/librpmb.so:$(TARGET_COPY_OUT_VENDOR)/lib64/librpmb.so \
